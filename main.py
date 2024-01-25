9921bbfaa7ec998a0c15745e268d4529459131e2
Satwik <satwik292007@gmail.com>
2024-01-25 19:35:08 +0530

    Created main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+'''
+Railway Reservation System:
+ - Functionality
+   - User can book tickets
+   - Food reservation
+   - View train position in real time
+   - Choose seats
+   - Crdentials for booking ticket
+   - Train positions updated in real time
+'''
+
+import json
+
+class Train:
+    current_location = None
+    destination = None
+    
+
+class JSONHandler:
+    ...
+
+
