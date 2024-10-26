f0bc271e5bc0c18c05eeb98b87dba495fcf2a0a1
Satwik <satwik292007@gmail.com>
2024-10-27 02:04:05 +0530

    Fix errors in queries.

--- query.sql
+++ query.sql
@@ -1,3 +1,4 @@
+-- DROP TABLE IF EXISTS users;
 CREATE TABLE Users (
     password VARCHAR(10) NOT NULL,
     username VARCHAR(50) NOT NULL UNIQUE,
@@ -8,6 +9,8 @@ CREATE TABLE Users (
     PRIMARY KEY(username)
 );
 
+
+-- DROP TABLE IF EXISTS Account;
 CREATE TABLE Account (
     balance INT NOT NULL CHECK (balance > 0),
     created DATE NOT NULL,
@@ -18,26 +21,29 @@ CREATE TABLE Account (
     ON UPDATE CASCADE
 );
 
+-- DROP TABLE IF EXISTS FixedDepo;
 CREATE TABLE FixedDepo (
-    fdID INT AUTO INCREMENT,
-    fdName VARCHAR(100) DEFAULT 'Fixed Deposit',
+    fdID INT AUTO_INCREMENT,
+    fdName VARCHAR(30) DEFAULT 'Fixed Deposit',
     username VARCHAR(50) NOT NULL UNIQUE,
     principal INT NOT NULL CHECK(principal > 0),
     interest INT NOT NULL CHECK(interest > 0),
     maturedate DATE NOT NULL,
     matured TINYINT(1) NOT NULL DEFAULT 0,
+    PRIMARY KEY(fdID),
     FOREIGN KEY(username) REFERENCES Users(username)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
+-- DROP TABLE Transactions;
 CREATE TABLE Transactions (
-    transID INT AUTO INCREMENT,
+    transID INT AUTO_INCREMENT,
     payerID VARCHAR(50) NOT NULL UNIQUE,
     receieverID VARCHAR(50) NOT NULL UNIQUE,
     transDate DATE,
     amount INT NOT NULL CHECK(amount > 0),
-    comment TINYTEXT DEFAULT "Empty",
+    comment TINYTEXT,
     PRIMARY KEY(transID),
     FOREIGN KEY(payerID) REFERENCES Users(username)
     ON DELETE RESTRICT
@@ -47,8 +53,10 @@ CREATE TABLE Transactions (
     ON UPDATE CASCADE
 );
 
+-- DROP TABLE Updates;
 CREATE TABLE Updates (
     username VARCHAR(50) NOT NULL,
     content TINYTEXT NOT NULL,
-    date DATE,
+    updateDate DATE,
+    FOREIGN KEY(username) REFERENCES Users(username)
 );
