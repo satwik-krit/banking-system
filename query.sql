51df1e39d6e6518a80088960ece23420d29212ac
Satwik <satwik292007@gmail.com>
2024-10-27 01:18:34 +0530

    Chnages to db schema.

--- query.sql
+++ query.sql
@@ -1,6 +1,10 @@
 CREATE TABLE Users (
     password VARCHAR(10) NOT NULL,
     username VARCHAR(50) NOT NULL UNIQUE,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    age INT NOT NULL CHECK (age > 0),
+    inactive TINYINT(1) NOT NULL DEFAULT 0,
     PRIMARY KEY(username)
 );
 
@@ -15,7 +19,7 @@ CREATE TABLE Account (
 );
 
 CREATE TABLE FixedDepo (
-    fdid INT AUTO INCREMENT,
+    fdID INT AUTO INCREMENT,
     username VARCHAR(50) NOT NULL UNIQUE,
     principal INT NOT NULL CHECK(principal > 0),
     interest INT NOT NULL CHECK(interest > 0),
@@ -24,4 +28,26 @@ CREATE TABLE FixedDepo (
     FOREIGN KEY(username) REFERENCES Users(username)
     ON DELETE CASCADE
     ON UPDATE CASCADE
+);
+
+CREATE TABLE Transactions (
+    transID INT AUTO INCREMENT,
+    payerID VARCHAR(50) NOT NULL UNIQUE,
+    receieverID VARCHAR(50) NOT NULL UNIQUE,
+    transDate DATE,
+    amount INT NOT NULL CHECK(amount > 0),
+    comment TINYTEXT DEFAULT "Empty",
+    PRIMARY KEY(transID),
+    FOREIGN KEY(payerID) REFERENCES Users(username)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+    FOREIGN KEY(payerID) REFERENCES Users(username)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE Updates (
+    username VARCHAR(50) NOT NULL,
+    content TINYTEXT NOT NULL,
+    date DATE,
 );
